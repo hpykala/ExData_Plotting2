986e93aa33405a144c0edfd268165c33a717fa9e
Heli Pykälä <heli.pykala@iki.fi>
2016-04-22 13:03:43 +0300

    Plot1

---
+++ plot1.R
@@ -0,0 +1,21 @@
+if(!file.exists("summarySCC_PM25.rds")) {
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip", "FNEI_data.zip", mode="wb")
+    unzip("FNEI_data.zip")
+}
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+sums <- tapply(NEI$Emissions,NEI$year,sum)
+yearly <- data.frame(year = as.numeric(names(sum)), pm25tot = sum/(10^6))
+
+png("plot1.png")
+plot(yearly$year, 
+     yearly$pm25tot,
+     pch = 19,
+     xlab = "Year", 
+     ylab = "Total PM2.5 emissions (Million tons)", 
+     main = "Total yearly pm2.5 emissions")
+abline(lm(pm25tot ~ year, data = yearly))
+
+dev.off()
